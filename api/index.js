0cc4342ccd7c5ac007e46751ca4231c5bb617e20
Anu Anil <anuanildilva@gmail.com>
2023-11-26 22:54:49 +0400

    commit

--- api/index.js
+++ api/index.js
@@ -1,7 +1,9 @@
 import express from 'express'
+import mongoose from 'mongoose'
+mongoose.connect("mongodb+srv://anuanil:lovecoding@cluster0.4j8yin6.mongodb.net/redux-web-app?retryWrites=true&w=majority")
 
 const app = express();
 
 app.listen(3000, () => {
-    console.log('Server listening on port 3000!!')
+    console.log('Server listening on port 3000...')
 })
