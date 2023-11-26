4742e24365b88e5ab4a1ebd886816ec0cbe5cbda
Anu Anil <anuanildilva@gmail.com>
2023-11-26 23:09:38 +0400

    mongodb

--- api/index.js
+++ api/index.js
@@ -1,9 +1,16 @@
 import express from 'express'
 import mongoose from 'mongoose'
-mongoose.connect("mongodb+srv://anuanil:lovecoding@cluster0.4j8yin6.mongodb.net/redux-web-app?retryWrites=true&w=majority")
+import dotenv from 'dotenv'
+dotenv.config()
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log('connected to MongoDB')
+})
+.catch((err)=>{
+    console.log(err)
+})
 
 const app = express();
 
 app.listen(3000, () => {
-    console.log('Server listening on port 3000...')
+    console.log('Server listening on port 3000')
 })
